083bec49fd0e09798b1efa557dc5de1dc83fa735
Yuda531 <yudaagung70@gmail.com>
2024-12-11 10:37:03 +0700

    Cleaning Code(Remove Unused Code)

--- app/src/main/java/com/dicoding/thriftify/ui/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/thriftify/ui/main/MainActivity.kt
@@ -39,7 +39,6 @@ class MainActivity : AppCompatActivity() {
             val appBarConfiguration = AppBarConfiguration(
                 setOf(
                     R.id.navigation_home,
-                    R.id.navigation_search,
                     R.id.navigation_account
                 )
             )
